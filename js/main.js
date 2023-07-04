1ce5a5ffda6ab062c3fc7527f3d07159ac80f215
stefanenache12 <stefanenache12@gmail.com>
2023-07-04 15:02:10 +0300

    END MILESTONE 3

--- js/main.js
+++ js/main.js
@@ -3,7 +3,50 @@ const { createApp } = Vue;
 createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        newTask: '',
+        tasks: [
+          {
+            name: 'Go to the grocery',
+            done: false,
+          },
+          {
+            name: 'Pay the bill',
+            done: true,
+          },
+          {
+            name: 'Wash the car',
+            done: false,
+          }
+        ]
       }
-    }
+    },
+    methods: {
+
+      addTask(){
+          if(this.newTask.trim() != ''){
+            
+            const newTask = {
+              name: this.newTask,
+              done: false
+            };
+
+            this.tasks.push(newTask);
+
+            this.newTask = '';
+          };
+      },
+
+      removeTask(index){
+        this.tasks.splice(index,1)
+      },
+
+      markAsDone(singleTask){
+
+        if(singleTask.done == false) {
+          singleTask.done = true;
+        } else{
+          singleTask.done = false;
+        }
+      }                                                                          
+  }
   }).mount('#app');
